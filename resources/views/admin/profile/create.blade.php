818606a8eade15c6200b9df05652707d6e97be28
”takanuki” <genka8725@gmail.com>
2020-08-21 12:30:09 +0000

    Add:Laravel 14 課題6(17行目-19行目を追加)

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -12,11 +12,18 @@
             <div class="col-md-8 mx-auto">
                 <h2>プロフィール新規作成</h2>
                 <form action="{{ action('Admin\ProfileController@create') }}" method="post">
-                    <input type="text" name="name" value="氏名">
-                    <input type="text" name="gender" value="性別">
-                    <input type="text" name="hobby" value="趣味">
-                    <input type="text" name="introduction" value="自己紹介欄">
-                    <input type="submit" name="送信">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    氏名：<input type="text" name="name" value="">
+                    性別：<input type="text" name="gender" value="">
+                    趣味：<input type="text" name="hobby" value="">
+                    自己紹介欄：<textarea name="introduction" rows="4" cols="40"></textarea>
+                    <input type="submit" name="投稿">
                 </form>
             </div>
         </div>
