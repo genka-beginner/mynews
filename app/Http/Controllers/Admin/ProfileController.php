2355af8a40ed57dd22a4b4985ffed74ca2a64262
”takanuki” <genka8725@gmail.com>
2020-09-23 12:01:03 +0000

    Add:Laravel16 課題1 admin/profile/createから送信された情報の保存

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -12,18 +14,41 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
-    {
+     public function create(Request $request){
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
         return redirect('admin/profile/create');
     }
 
-    public function edit()
-    {
-        return view('admin.profile.edit');
+    public function edit(Request $request){
+        $profile = Profile::find($request->id);
+        if(empty($profile)){
+        abort(404);
+        }
+        return view('admin.profile.edit',['profile_form' => $profile]);
     }
 
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+    public function update(Request $request){
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        // フォームから送信されてきた_tokenを削除する
+        unset($profile_form['_token']);
+        
+        // データベースに保存する
+        $profile->fill($profile_form);
+        $profile->save();
+        return redirect('admin/profile/create');
+  }
+  
+  
+  
 }
