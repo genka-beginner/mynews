e728efba10ce136e9b3a7f31febf93011822f4e8
”takanuki” <genka8725@gmail.com>
2020-11-16 05:04:46 +0000

    ファイル最新化

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\HTML;
-use app\profile;
+use App\Profile;
 
 class ProfileController extends Controller
 {
